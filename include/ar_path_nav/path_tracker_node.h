e2a22bc312b41f06dcbaa1c1883f5e42148822eb
JunsoMin <minjs4562@gmail.com>
2024-08-19 14:43:28 +0900

    fix: timer callback, feat: odom subscriber

--- include/ar_path_nav/path_tracker_node.h
+++ include/ar_path_nav/path_tracker_node.h
@@ -3,9 +3,12 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Quaternion.h>
 #include <nav_msgs/Path.h>
-
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
 #include <xycar_msgs/xycar_motor.h>
+
 
 class PathTracker
 {
@@ -16,6 +19,7 @@ private:
   int get_closest_idx();
   
   void path_callback(const nav_msgs::Path::ConstPtr& msg);
+  void odom_callback(const nav_msgs::Odometry::ConstPtr& msg);
   void timer_callback(const ros::TimerEvent& );
   void set_goal();
   void stenly();
@@ -23,7 +27,10 @@ private:
   ros::NodeHandle nh_;
   ros::NodeHandle priv_nh_;
 
+  ros::Timer timer_;
+
   ros::Subscriber path_subs_;
+  ros::Subscriber odom_subs_;
   ros::Publisher control_pub_;
 
   nav_msgs::Path path_;
@@ -31,6 +38,9 @@ private:
 
   double k_;
   double velocity_;
+  double curr_x_;
+  double curr_y_;
 
+  geometry_msgs::Quaternion curr_ori_;
 };
 #endif  // AR_PATH_NAV__PATH_TRACKER_H_
--- src/path_tracker_node.cpp
+++ src/path_tracker_node.cpp
@@ -1,50 +1,60 @@
 #include "ar_path_nav/path_tracker_node.h"
+#include <tf/transform_datatypes.h>  // To use for quaternion to yaw conversion
 
 PathTracker::PathTracker(ros::NodeHandle nh_)
-:nh_(nh_),priv_nh_("~")
+: nh_(nh_), priv_nh_("~"), velocity_(1.0), k_(1.0)
 {
-  path_subs_ = nh_.subscribe("/ar_path", 10, &PathTracker::path_callback ,this);
+  path_subs_ = nh_.subscribe("/ar_path", 10, &PathTracker::path_callback, this);
   control_pub_ = nh_.advertise<xycar_msgs::xycar_motor>("/xycar_motor", 10);
 
-  ros::Timer timer = nh_.createTimer(ros::Duration(0.1), &PathTracker::timer_callback, this);
+  timer_ = nh_.createTimer(ros::Duration(0.1), &PathTracker::timer_callback, this);
+
+  odom_subs_ = nh_.subscribe("/odom", 10, &PathTracker::odom_callback, this);
 }
 
-void PathTracker::path_callback(const nav_msgs::Path::ConstPtr& msg){
+void PathTracker::odom_callback(const nav_msgs::Odometry::ConstPtr& msg) {
+  curr_x_ = msg->pose.pose.position.x;
+  curr_y_ = msg->pose.pose.position.y;
+  curr_ori_ = msg->pose.pose.orientation;
+}
+
+void PathTracker::path_callback(const nav_msgs::Path::ConstPtr& msg) {
   path_.header = msg->header;
   path_.poses = msg->poses;
 }
 
-void PathTracker::timer_callback(const ros::TimerEvent&)
-{
+void PathTracker::timer_callback(const ros::TimerEvent&) {
+  if (path_.poses.empty()) {
+    ROS_WARN("Received empty path, skipping this cycle.");
+    return;
+  }
+
   set_goal();
   stenly();
 }
 
-void PathTracker::set_goal()
-{
+void PathTracker::set_goal() {
   int closest_idx = get_closest_idx();
   int goal_idx = closest_idx;
 
-  if (goal_idx >= path_.poses.size())
-  {
+  if (goal_idx >= path_.poses.size()) {
     goal_idx = path_.poses.size() - 1;
   }
 
   goal_pose_ = path_.poses[goal_idx].pose;
 }
 
-int PathTracker::get_closest_idx()
-{
+int PathTracker::get_closest_idx() {
   double min_dist = std::numeric_limits<double>::max();
   int closest_idx = 0;
 
-  for (size_t i = 0; i < path_.poses.size(); ++i)
-  {
-    double dx = path_.poses[i].pose.position.x;
-    double dy = path_.poses[i].pose.position.y;
+  for (size_t i = 0; i < path_.poses.size(); ++i) {
+    double dx = curr_x_ - path_.poses[i].pose.position.x;
+    double dy = curr_y_ - path_.poses[i].pose.position.y;
+
     double dist = std::sqrt(dx * dx + dy * dy);
 
-    if (dist < min_dist){
+    if (dist < min_dist) {
       min_dist = dist;
       closest_idx = i;
     }
@@ -53,19 +63,28 @@ int PathTracker::get_closest_idx()
   return closest_idx;
 }
 
-void PathTracker::stenly()
-{
-  double dx = goal_pose_.position.x;
-  double dy = goal_pose_.position.y;
+void PathTracker::stenly() {
+  double dx = goal_pose_.position.x - curr_x_;
+  double dy = goal_pose_.position.y - curr_y_;
 
   double heading_to_goal = std::atan2(dy, dx);
-  double current_yaw = 0.0;
 
-  double heading_error = heading_to_goal - current_yaw;
+  tf::Quaternion q(
+      curr_ori_.x,
+      curr_ori_.y,
+      curr_ori_.z,
+      curr_ori_.w);
+  tf::Matrix3x3 m(q);
+  double roll, pitch, yaw;
+  m.getRPY(roll, pitch, yaw);
+
+  double heading_error = heading_to_goal - yaw;
   double cross_track_error = std::sqrt(dx * dx + dy * dy);
-  if(heading_error < 0){
+  
+  if (heading_error < 0) {
     cross_track_error = -cross_track_error;
   }
+
   double control_steering = heading_error + std::atan2(k_ * cross_track_error, velocity_);
 
   xycar_msgs::xycar_motor motor_msg;
@@ -75,8 +94,7 @@ void PathTracker::stenly()
   control_pub_.publish(motor_msg);
 }
 
-
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
   ros::init(argc, argv, "path_tracker_node");
   ros::NodeHandle nh;
 
