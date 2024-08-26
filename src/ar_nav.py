6acc87affe2b601c3e8c1042bb4ae63a5b3a379d
JunseoMin <minjs4562@gmail.com>
2024-08-26 17:16:03 +0000

    feat: traffilight fin. 0827

---
+++ src/ar_nav.py
@@ -0,0 +1,252 @@
+# -*- coding: utf-8 -*-
+
+import rospy 
+import math
+import numpy as np 
+from nav_msgs.msg import Path, Odometry
+from geometry_msgs.msg import PointStamped, Point
+import tf
+from math import cos, sin, sqrt, atan2
+from tf.transformations import euler_from_quaternion
+from ar_track_alvar_msgs.msg import AlvarMarkers
+from xycar_msgs.msg import xycar_motor
+import time
+
+
+def Can2BaseT():
+    
+    cosR, sinR = cos(-math.pi / 2), sin(-math.pi / 2)
+    cosY, sinY = cos(-math.pi / 2), sin(-math.pi / 2)
+
+    rotRoll = np.array([1,0,0, 0,cosR,-sinR, 0,sinR,cosR]).reshape(3,3)
+    rotYaw = np.array([cosY,-sinY,0, sinY,cosY,0, 0,0,1]).reshape(3,3)
+    
+    rotMat = rotYaw@rotRoll
+    
+    cam_trans = np.array([[0.2, 0, 0.05]])
+    Tr_cam_to_vehicle = np.concatenate((rotMat,cam_trans.T),axis = 1)
+    Tr_cam_to_vehicle = np.insert(Tr_cam_to_vehicle, 3, values=[0,0,0,1],axis = 0)
+    
+    return Tr_cam_to_vehicle
+
+
+class ArDriving:
+    def __init__(self):
+        rospy.init_node('ar_drive', anonymous=True)
+        self.ar_flag = -1
+
+        self.distance_diff = 0.0
+        self.dist_before = 0.0
+
+        rospy.Subscriber('/ar_pose_marker', AlvarMarkers, self.ar_callback)
+        rospy.Subscriber('/odom', Odometry, self.odom_callback)
+
+        self.ar_point_pub = rospy.Publisher('/ar_point', PointStamped, queue_size=1)
+        self.cmd_pub = rospy.Publisher('/xycar_motor', xycar_motor, queue_size=1)
+
+        self.is_ar = False
+        self.is_odom = False
+        self.is_path = False
+        self.obs_mode = True
+        self.ar_data = None
+        self.cam2base = Can2BaseT()
+        self.ar_list = []
+        self.target_ar = None
+        self.target_x_offset = 0.0
+        self.target_y_offset = 0.5
+        self.odom_yaw = 0
+        self.ar_ids = []
+        self.tmp_id = -1
+
+        self.last_marker_ts = rospy.Time()
+        self.marker_timeout = rospy.Duration(5.5)
+
+        self.heading_pid_error_1 = 0.0
+        self.heading_pid_ui_1 = 0.0
+        self.distance_pid_error_1 = 0.0
+        self.distance_pid_ui_1 = 0.0
+
+        self.cmd_msg = xycar_motor()
+
+        self.init = True
+
+        rate = rospy.Rate(30)
+
+        while not rospy.is_shutdown():
+            if self.is_ar and self.is_odom:
+
+                target_ar_pointstamped = PointStamped()
+                target_ar_pointstamped.header.frame_id = 'base_link'
+
+                odom_ar_pointstamped = PointStamped()
+                odom_ar_pointstamped.header.frame_id = 'odom'
+
+                if len(self.ar_list) == 0:
+                    print("no ar mark!")
+                    
+                else:
+                    target_ar = self.find_target_ar()   
+                    ar_pose = np.array([target_ar.pose.pose.position.x, target_ar.pose.pose.position.y, target_ar.pose.pose.position.z, 1]).T
+                    base_ar_pose = self.cam2base.dot(ar_pose)
+                    # print(base_ar_pose)
+
+                    target_x = base_ar_pose[0] + self.target_x_offset
+                    target_y = base_ar_pose[1] + self.target_y_offset
+
+                    ar_yaw = math.atan(target_y / target_x)
+                    distance = sqrt(target_x ** 2 + target_y ** 2)
+
+                    self.odom_ar_point = Point()
+                    self.odom_ar_point.x = self.cur_odom.x + distance * cos(self.odom_yaw + ar_yaw)
+                    self.odom_ar_point.y = self.cur_odom.y + distance * sin(self.odom_yaw + ar_yaw)
+                    self.odom_ar_point.z = 0
+                    odom_ar_pointstamped.point = self.odom_ar_point
+                    self.last_marker_ts = rospy.Time()
+
+                    self.update_marker_angle_distance()
+                
+                print("current flag:", self.ar_flag)
+                self.update_cmd()
+                self.ar_point_pub.publish(odom_ar_pointstamped)
+
+                if self.ar_flag == 2:
+                    if self.init:
+                        self.cmd_msg.angle = -30
+                        self.init = False
+                        rospy.logwarn("!! Second flag !!")
+                        rospy.logwarn("!! angle : -30 !!")
+
+                        start_time = rospy.Time.now()
+                        while rospy.Time.now() - start_time < rospy.Duration(1.5):
+                            self.cmd_pub.publish(self.cmd_msg)
+                            rospy.sleep(0.1)  # Sleep for a short duration to avoid spamming
+
+
+                self.cmd_pub.publish(self.cmd_msg)
+            
+            rate.sleep()
+
+    def pid_distance(self):
+        # PID controller to control the linear velocity based on distance
+        ts = 0.2
+        kp = 0.6
+        ki = 2.15
+        kd = 0.3
+        desired_distance = 0.5
+        error = desired_distance - self.marker_distance
+
+        if error < 0.45:
+            error = -error
+
+        up = kp*error
+        ui = ki*error + self.distance_pid_ui_1*ts
+        ud = (kd/ts)*(error-self.distance_pid_error_1)
+        u = up + ui + ud
+
+        self.distance_pid_error_1 = error
+        self.distance_pid_ui_1 = ui
+
+        # rospy.loginfo(f"ARTag_distance: {self.marker_distance}")
+
+        print(u)
+        return u
+    
+    def pid_heading(self):
+        # PID controller to control the angular velocity based on heading
+        ts = 0.8
+        kp = 0.1
+        ki = 0.20
+        kd = 1.2
+        error = self.marker_angle
+
+        up = kp*error
+        ui = ki*error + self.heading_pid_ui_1*ts
+        ud = (kd/ts)*(error-self.heading_pid_error_1)
+        u = up + ui + ud
+
+        self.heading_pid_error_1 = error
+        self.heading_pid_ui_1 = ui
+        print("angle: ", u / math.pi * 180)
+        return u
+
+    def update_cmd(self):
+        
+        if len(self.ar_list) == 0:
+            print("just go straight")
+            self.cmd_msg.speed = 3
+            self.cmd_msg.angle = int(0.0)
+            return
+
+        lin_cmd = self.pid_distance()
+        ang_cmd = self.pid_heading()
+
+        ang_cmd = ang_cmd * 180 / math.pi
+
+        if self.marker_distance < 1:
+            ang_cmd += 25
+
+        if ang_cmd > 50:
+            ang_cmd = 50
+        elif ang_cmd < -50:
+            ang_cmd = -50
+            
+        self.cmd_msg.angle =  -int(ang_cmd)
+        self.cmd_msg.speed = int(lin_cmd) + 3
+
+    def update_marker_angle_distance(self):
+        if self.odom_ar_point:
+            position_x = self.odom_ar_point.x - self.cur_odom.x
+            position_y = self.odom_ar_point.y - self.cur_odom.y
+            self.marker_angle = math.atan(position_y / position_x) - self.odom_yaw
+            self.marker_distance = math.sqrt(
+                position_x * position_x + position_y * position_y
+            )
+
+    def odom_callback(self, msg):
+        self.is_odom = True
+        self.cur_odom = msg.pose.pose.position
+        orient = msg.pose.pose.orientation
+        self.odom_yaw = tf.transformations.euler_from_quaternion([orient.x, orient.y, orient.z, orient.w])[2]
+        # print(self.odom_yaw * 180.0 / math.pi)
+
+    def ar_callback(self, msg):
+        self.is_ar = True
+        ar_list = []
+        for ar in msg.markers:
+            ar_list.append(ar)
+        
+        self.ar_list = ar_list
+
+
+    def find_target_ar(self):
+        min_dist = float("inf")
+        min_idx = -1
+        dist = 0.
+
+        for i, ar in enumerate(self.ar_list):
+            dist = ar.pose.pose.position.x ** 2 + ar.pose.pose.position.y ** 2 + ar.pose.pose.position.z ** 2
+            if min_dist > dist:
+                min_dist = dist
+                min_idx = i
+            self.last_marker_ts = ar.header.stamp
+
+        target_ar = self.ar_list[min_idx]
+        print(len(self.ar_list))
+        print("current target :",target_ar.id )
+        distance_diff = abs(self.dist_before - min_dist) 
+
+        if distance_diff > 1.:
+            self.ar_flag += 1
+        
+        self.dist_before = min_dist
+
+        # odom_quaternion=(target_ar.pose.pose.orientation.x,target_ar.pose.pose.orientation.y,target_ar.pose.pose.orientation.z,target_ar.pose.pose.orientation.w)
+        # roll,pitch,yaw=euler_from_quaternion(odom_quaternion)
+        # print(target_ar.id, roll/math.pi*180,pitch/math.pi*180 ,yaw/math.pi*180 )
+        return target_ar
+
+if __name__ == '__main__':
+    try:
+        ArDriving()
+    except rospy.ROSInterruptException:
+        pass
---
+++ src/trafficlight.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import rospy
+from xycar_msgs.msg import xycar_motor
+from ar_track_alvar_msgs.msg import AlvarMarkers
+from ar_track_alvar_msgs.msg import AlvarMarker
+import math
+import numpy as np
+from math import cos,sin
+from sensor_msgs.msg import Image
+import cv2
+
+import sys
+
+def Cam2BaseT():
+    
+    cosR, sinR = cos(-math.pi / 2), sin(-math.pi / 2)
+    cosY, sinY = cos(-math.pi / 2), sin(-math.pi / 2)
+
+    rotRoll = np.array([1,0,0, 0,cosR,-sinR, 0,sinR,cosR]).reshape(3,3)
+    rotYaw = np.array([cosY,-sinY,0, sinY,cosY,0, 0,0,1]).reshape(3,3)
+    
+    rotMat = rotYaw@rotRoll
+    
+    cam_trans = np.array([[0.2, 0, 0.05]])
+    Tr_cam_to_vehicle = np.concatenate((rotMat,cam_trans.T),axis = 1)
+    Tr_cam_to_vehicle = np.insert(Tr_cam_to_vehicle, 3, values=[0,0,0,1],axis = 0)
+    
+    return Tr_cam_to_vehicle
+
+class TrafficLight:
+    def __init__(self):
+        rospy.init_node('trafficlight_node', anonymous=True)
+
+        rospy.Subscriber("/ar_pose_marker",AlvarMarkers, self.ar_callback)
+        rospy.Subscriber("/usb_cam/image_raw",Image, self.image_callback)
+        motor_pub = rospy.Publisher("/xycar_motor",xycar_motor,queue_size=1)
+        
+        #flags for algorithm
+        self.ar_subscribed = False
+        self.trafficlight_end = False
+        self.arrived = False
+
+        self.goal_marker = AlvarMarker()
+        self.markers = []
+        self.motor_msg = xycar_motor()
+        """
+        xycar motor:
+        speed 3 ~ 6
+        angle -50 ~ +50
+        """
+        self.T_cb = Cam2BaseT()
+
+        # target margin
+        self.offset_x = 0.
+        self.offset_y = 0.
+
+        # target position (ref: baselink)
+        self.target_x = 0.
+        self.target_y = 0.
+        # target distance (ref: baselink)
+        self.distance = 0.
+        # target yaw (ref: baselink)
+        self.ar_yaw = 0.
+
+        # D gain arg save
+        self.heading_pid_ui_1 = 0.
+        self.heading_pid_error_1 = 0.
+        self.distance_pid_ui_1 = 0.
+        self.distance_pid_error_1 = 0.
+
+        ######### light detect params ########
+        self.img_curr = Image()
+
+        rate = rospy.Rate(10)
+        
+        while not rospy.is_shutdown():
+            if not self.ar_subscribed:
+                # rospy.logwarn("AR not subscribed wait...")
+                continue
+
+            if self.trafficlight_end:
+                rospy.signal_shutdown("Green light. End this node!!")
+
+            if self.arrived:
+                # jump this loop if target distance is small enough
+                self.check_greenlight() # set trafficlight_end == true if green light detected
+                rospy.logwarn("arrived to trafficlight Waiting greenlight ...")
+                rospy.logwarn("current distance: %f", self.distance)
+                self.motor_msg.speed = 0
+                self.motor_msg.angle = 0
+                motor_pub.publish(self.motor_msg)
+                continue
+            
+            # move to ar marker by pid
+            self.update_target_marker() # update target marker
+            self.update_target_pose()   # target xy set
+            self.pid_steering()
+            self.pid_distance()
+
+            motor_pub.publish(self.motor_msg)
+
+            if self.distance < 1.5:
+                rospy.loginfo("arrived!! distance: %d", self.distance)
+                self.arrived = True
+            rate.sleep()
+    
+    def ar_callback(self,msg):
+        # save ar markers
+        self.markers = msg.markers
+
+        if len(self.markers):
+            self.ar_subscribed = True
+        else:
+            self.ar_subscribed = False
+
+    def image_callback(self,msg):
+        self.img_curr = msg
+        rospy.loginfo("image subscribed")
+    
+    def update_target_marker(self):
+        print(self.markers)
+
+        distance = 0.
+        min_distance = float('inf')
+
+        for mark in self.markers:
+            dx = mark.pose.pose.position.x
+            dy = mark.pose.pose.position.y
+            dz = mark.pose.pose.position.z
+            distance = dx ** 2 + dy ** 2 + dz ** 2
+
+            if distance < min_distance:
+                min_distance = distance
+                self.goal_marker = mark
+            
+            print("********************************")
+            print("closest marker update! id: ", mark.id)
+            print("closest marker update! distance: ", math.sqrt(min_distance))
+            print("********************************")
+        
+
+
+    def update_target_pose(self):
+        # get ar pose referenced to baselink
+        if self.goal_marker.id == '':
+            rospy.logwarn("No marker!!")
+            return
+
+        tmp_pose = np.array([self.goal_marker.pose.pose.position.x,self.goal_marker.pose.pose.position.y,self.goal_marker.pose.pose.position.z,1]).T
+        goal_pose_b = self.T_cb.dot(tmp_pose)
+
+        self.target_x = goal_pose_b[0] + self.offset_x
+        self.target_y = goal_pose_b[1] + self.offset_y
+
+        self.ar_yaw = math.atan(self.target_y/self.target_x)
+        self.distance = math.sqrt(pow(self.target_x,2) + pow(self.target_y,2))
+
+    def pid_steering(self):
+        # calc pid control for steering
+        ts = 0.8
+        kp = 0.1
+        ki = 0.20
+        kd = 1.2
+
+        error = self.ar_yaw
+
+        up = kp*error
+        ui = ki*error + self.heading_pid_ui_1*ts
+        ud = (kd/ts)*(error-self.heading_pid_error_1)
+        u = up + ui + ud
+        
+        self.heading_pid_error_1 = error
+        self.heading_pid_ui_1 = ui
+        print("angle: ", u / math.pi * 180)
+
+        self.motor_msg.angle = int(u / math.pi * 180)
+    
+    def pid_distance(self):
+        # PID controller to control the linear velocity based on distance
+        ts = 0.2
+        kp = 0.6
+        ki = 2.15
+        kd = 0.3
+        desired_distance = 0.1
+        error = desired_distance - self.distance
+
+        if error < 0.45:
+            error = -error
+
+        up = kp*error
+        ui = ki*error + self.distance_pid_ui_1*ts
+        ud = (kd/ts)*(error-self.distance_pid_error_1)
+        u = up + ui + ud
+
+        self.distance_pid_error_1 = error
+        self.distance_pid_ui_1 = ui
+
+        print("speed: ", int(u + 3.0) )
+        self.motor_msg.speed = int(u + 3.0)
+    
+    def check_greenlight(self):
+        try:
+            cv_image = np.frombuffer(self.img_curr.data, dtype=np.uint8).reshape(self.img_curr.height, self.img_curr.width, -1)
+            cv_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2BGR)
+            cv2.imshow("original image",cv_image)
+
+            height, width, _ = cv_image.shape
+            top_part = cv_image[0:height // 3, :, :]
+
+            hsv = cv2.cvtColor(top_part, cv2.COLOR_BGR2HSV)
+            lower_green = np.array([40, 40, 40])
+            upper_green = np.array([80, 255, 255])
+            mask = cv2.inRange(hsv, lower_green, upper_green)
+
+            cv2.imshow("greenmask img",mask)
+            cv2.waitKey(0)
+
+            green_ratio = np.sum(mask > 0) / (mask.shape[0] * mask.shape[1])
+
+            threshold = 0.005
+            print("------------------")
+            print("current green: ", green_ratio)
+            print("------------------")
+
+            if green_ratio > threshold:
+                rospy.loginfo("Green light detected! Ending process.")
+                self.trafficlight_end = True
+            else:
+                rospy.loginfo("No green light detected.")
+
+        except Exception as e:
+            rospy.logerr("Error in processing image: %s", str(e))
+
+
+if __name__ == '__main__':
+    try:
+        TrafficLight()
+    except rospy.ROSInterruptException:
+        pass
